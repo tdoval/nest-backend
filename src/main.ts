52937ecb6d579a3c1e7e727bed77b6f0fa2e7f34
tdoval <thiagodovalaraujo@gmail.com>
2024-02-01 20:24:41 -0300

    @fix/ CORS adjustments

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Accept',
+  });
+
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
